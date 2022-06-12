536805ad8f3eeebb77a13e61e8915fa6a75f9938
hanxian韩闲 <woow.wu7@gmail.com>
2022-06-12 15:42:39 +0800

    feat: vnode.componentOptions.tag .Ctor.options.name.

--- packages/container/src/main.vue
+++ packages/container/src/main.vue
@@ -9,16 +9,16 @@
 // - 用于布局的容器组件，便于搭建页面的基本结构
 // 1
 // <el-container> 外层容器
-// a排列方式
+// a.排列方式
 //  - 在不设置属性 direction 时
 //    - 垂直排列：当 ( <el-container>  ) 子元素中包含 ( <el-header> 或 <el-footer> ) 时，全部子元素会垂直上下排列
 //    - 水平排列：当子元素中不包含<el-header>或<el-footer>时水平排列
 //  - 在设置了 direction 时，按设置的值进行排列，direction的值只能是 ( horizontal 或 vertical )
-// b思考
+// b.思考
 //  - 问题：为什么要有水平和垂直排列的区别呢？
 //  - 回答：因为要实现多不同的布局的组合方式
-// c是否支持嵌套
-//  - <el-container> 是可以嵌套 <el-container> 的
+// c.是否支持嵌套
+//  - <el-container> 是可以嵌套 <el-container> 的，在嵌套的<el-container>内又可以通过不同组合实现不同的布局
 
 // 2
 // <el-container> ------------------- section 标签
@@ -39,6 +39,8 @@
 // - direction
 //  - 表示：子元素的排列方向	string	horizontal / vertical
 //  - 默认值：子元素中有 el-header 或 el-footer 时为 vertical，否则为 horizontal
+//  - vertical 竖直的
+//  - horizontal 水平的
 
 // 5
 // <el-header>Header组件的属性 和 <el-footer>Footer组件的属性
@@ -52,9 +54,9 @@
 //    - 如果没有Aside和Main，必然占据整行
 //    - 如果有Aside和Main并且没有其他Container也会上下排列还是占据整行
 //    - 如果有Aside和Main并且还有同层级的Container-中，那么Header和Footer在这个同层级的Container-B中也会占据这个container-B的整行
-//    - 所以无论上面三种情况的那种，Heder和Footer都会占据Container的一整行，所以不需要width设置
-//    - 并且只要有 Header 和 Footer 就是 vertical 垂直排列的，则会占据整行
-
+//    - 所以:
+//      - 无论上面三种情况的那种，Heder和Footer都会占据Container的一整行，所以不需要width设置
+//      - 并且只要有 Header 和 Footer 就是 vertical 垂直排列的，则会占据整行
 
 // 6
 // <el-aside> Aside组件的属性
@@ -73,7 +75,7 @@ export default {
   componentName: "ElContainer",
 
   props: {
-    direction: String, // container 只有一个属性, direction
+    direction: String, // container 只有一个属性, direction，该参数会进行computed计算
   },
 
   computed: { // vertical 竖直的
@@ -95,6 +97,12 @@ export default {
             return tag === "el-header" || tag === "el-footer";
             // 这里表示，只要 <el-container> 的子组件 ( 注意是子组件，即第一层中的组件 ) 有 <el-header>或者<el-footer> 时就会垂直排列子组件
             // vnode.componentOptions.tag -> 用来获取组件的名称
+
+            // 扩展：获取 ( 组件名 ) 的方式有哪些？
+            // - 1. vnode.componentOptions.Ctor.options.name
+            // - 2. vnode.componentOptions.tag
+            // - 3. 在组件中，通过 this.$options.name
+            // - 区别：componentOptions.tag 为模版或者render书写的标签字符串时，根据用户的书写可能不统一
           })
         : false;
     },
