7470a01234ba236f805084994498327d71da3e21
woow_wu7 <woow.wu7@gmail.com>
2022-08-19 13:17:23 +0800

    feat: backtop.

--- packages/backtop/src/main.vue
+++ packages/backtop/src/main.vue
@@ -61,25 +61,25 @@ export default {
 
   mounted() {
     this.init();
-    this.throttledScrollHandler = throttle(300, this.onScroll);
-    this.container.addEventListener('scroll', this.throttledScrollHandler);
+    this.throttledScrollHandler = throttle(300, this.onScroll); // throttle
+    this.container.addEventListener('scroll', this.throttledScrollHandler); // 监听滚动事件
   },
 
   methods: {
     init() {
       this.container = document;
-      this.el = document.documentElement;
+      this.el = document.documentElement; // ---------------- 初始化时容器是 html
       if (this.target) {
-        this.el = document.querySelector(this.target);
+        this.el = document.querySelector(this.target); // --- 容器是 target
         if (!this.el) {
           throw new Error(`target is not existed: ${this.target}`);
         }
-        this.container = this.el;
+        this.container = this.el; // container
       }
     },
     onScroll() {
-      const scrollTop = this.el.scrollTop;
-      this.visible = scrollTop >= this.visibilityHeight;
+      const scrollTop = this.el.scrollTop; // 当前元素的垂直滚动条向下滚动的像素数量
+      this.visible = scrollTop >= this.visibilityHeight; // 显示时机
     },
     handleClick(e) {
       this.scrollToTop();
@@ -89,10 +89,17 @@ export default {
       const el = this.el;
       const beginTime = Date.now();
       const beginValue = el.scrollTop;
-      const rAF = window.requestAnimationFrame || (func => setTimeout(func, 16));
+      const rAF = window.requestAnimationFrame || (func => setTimeout(func, 16)); // requestAnimationFrame 的降级保护
       const frameFunc = () => {
         const progress = (Date.now() - beginTime) / 500;
-        if (progress < 1) {
+        if (progress < 1) { // 即 时间差小于500ms
+
+          // const cubic = value => Math.pow(value, 3); ---------- value的3次方，pow是次方的意思
+
+          // const easeInOutCubic = value => value < 0.5
+          //   ? cubic(value * 2) / 2
+          //   : 1 - cubic((1 - value) * 2) / 2;
+
           el.scrollTop = beginValue * (1 - easeInOutCubic(progress));
           rAF(frameFunc);
         } else {
